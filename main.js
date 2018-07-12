dcf3b09f2c01bfdc25a93a590923230e8f565e75
veringsek <veringsek@gmail.com>
2018-07-12 16:04:46 +0800

    .list with flex

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
         height: 768
     }); 
     win.loadURL(`file://${__dirname}/index.html`); 
-    // win.webContents.openDevTools(); 
+    win.webContents.openDevTools(); 
     win.on("closed", () => {
         win = null; 
     })
