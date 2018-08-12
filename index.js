3ef85a463dc88d02d4c8aa2b5e9b7f768e2723db
veringsek <veringsek@gmail.com>
2018-08-12 12:29:16 +0800

    only checked files are counted when counting their file number

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function addNewAction() {
-    var type = vmFiles.newAction.type; 
+    var type = vmBody.newAction.type; 
     switch (type) {
         case "replace": 
             addNewActionReplace(); 
@@ -10,9 +10,21 @@ function addNewAction() {
 function addNewActionReplace() {
     var pattern = txtNewActionReplacePattern.value; 
     var replacement = txtNewActionReplaceReplacement.value; 
-    vmFiles.actions.push({
+    vmBody.actions.push({
         type: "replace", 
+        checked: true, 
         pattern: pattern, 
         replacement: replacement
     }); 
+}
+
+function getFileNumber(index) {
+    var files = vmBody.files; 
+    var number = 0; 
+    for (var i = 0; i <= index; i++) {
+        if (files[i].checked) {
+            number += 1; 
+        }
+    }
+    return number; 
 }
