618bcc5fe54ec52ba5c4393af8969cf879970fa2
veringsek <veringsek@gmail.com>
2018-08-13 14:51:24 +0800

    check warning in preview

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function preview() {
     var number = 0; 
     for (var file of vm.files) {
-        if (file.checked) {
-            number += 1; 
-        }
-        file.number = number; 
         file.modified = file.name; 
         file.warning = "none"; 
+        if (!file.checked) {
+            continue; 
+        }
+        number += 1; 
+        file.number = number; 
         for (var action of vm.actions) {
             if (!action.checked) {
                 continue; 
@@ -15,6 +16,13 @@ function preview() {
         }
         if (file.modified === file.name) {
             file.warning = vm.warnings.raise("unchanged", file.warning); 
+        }
+        for (var other of vm.files) {
+            if (other === file) {
+                break; 
+            } else if (other.modified === file.modified) {
+                file.warning = vm.warnings.raise("conflict", file.warning); 
+            }
         }
     }
 }
