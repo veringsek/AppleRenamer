beba5566ec2889ef541a12dd0476c741ea54aca6
veringsek <veringsek@gmail.com>
2018-08-13 10:39:26 +0800

    use v-for to generate actionTypes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function addNewAction() {
-    var type = vmBody.newAction.type; 
+    var type = vm.newAction.type; 
     switch (type) {
         case "replace": 
             addNewActionReplace(); 
@@ -10,7 +10,7 @@ function addNewAction() {
 function addNewActionReplace() {
     var pattern = txtNewActionReplacePattern.value; 
     var replacement = txtNewActionReplaceReplacement.value; 
-    vmBody.actions.push({
+    vm.actions.push({
         type: "replace", 
         checked: true, 
         pattern: pattern, 
@@ -30,15 +30,15 @@ function modifyNameReplace(action, file) {
     file.modified = file.modified.replace(new RegExp(action.pattern), action.replacement); 
 }
 
-function previewFiles() {
+function preview() {
     var number = 0; 
-    for (var file of vmBody.files) {
+    for (var file of vm.files) {
         if (file.checked) {
             number += 1; 
         }
         file.number = number; 
         file.modified = file.name; 
-        for (var action of vmBody.actions) {
+        for (var action of vm.actions) {
             if (!action.checked) {
                 continue; 
             }
